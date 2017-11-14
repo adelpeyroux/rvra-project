8867a9efd9f82184251c21102a3a6ecab773ce99
sebpouteau <sebastien.pouteau@etu.u-bordeaux.fr>
2017-11-14 15:24:48 +0100

    nettoyage de maximilian

---
+++ ext/Maximilian/main.cpp
@@ -0,0 +1,22 @@
+#include "maximilian.h"
+#include "player.h"
+
+//This shows how the fundamental building block of digital audio - the sine wave.
+//
+maxiOsc mySine;//One oscillator - can be called anything. Can be any of the available waveforms.
+void setup() {//some inits
+    //nothing to go here this time
+}
+
+void play(double *output) {
+    
+    output[0]=mySine.sinewave(440);
+    output[1]=output[0];
+    
+}
+
+int main() {
+  start_player();
+  std::cout << "ici" << std::endl;
+  return 0;
+}
---
+++ ext/Maximilian/player.h
@@ -0,0 +1,22 @@
+/*
+ *  player.h
+ *  rtaudiotest
+ *
+ *  Created by Chris on 23/08/2011.
+ *  Copyright 2011 Goldsmiths Creative Computing. All rights reserved.
+ *
+ */
+
+#ifndef __PLAYER_H__
+#define __PLAYER_H__
+
+
+#define MAXIMILIAN_PORTAUDIO
+//#define MAXIMILIAN_RT_AUDIO
+
+void start_player();
+
+void stop_stream();
+
+
+#endif
