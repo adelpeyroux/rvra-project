c2548148dac03a4c7b82b6af9e522ac691a81633
Rack07 <rack.kendorim@gmail.com>
2017-12-05 13:51:07 +0100

    Auto stash before merge of "master" and "origin/master"

--- src/Interfaces.cpp
+++ src/Interfaces.cpp
@@ -13,8 +13,8 @@ using namespace std;
 //***************************************
 //***** Constructeurs / Destructeurs ****
 //***************************************
-Interfaces::Interfaces(int radiusCenter, int radiusButton, int nbsegments, bool segmentation, Scalar Color, double alpha)
-	: _RadiusCenter(radiusCenter), _RadiusButton(radiusButton), _Segmentation(segmentation), _Color(Color), _Alpha(alpha)
+Interfaces::Interfaces(int radiusCenter, int radiusButton, int nbsegments, bool segmentation, bool text, Scalar Color, double alpha)
+	: _RadiusCenter(radiusCenter), _RadiusButton(radiusButton), _Segmentation(segmentation), _Text(text), _Color(Color), _Alpha(alpha)
 {
 	SetNbSegments(nbsegments);
 }
@@ -100,16 +100,17 @@ void Interfaces::drawMarker(Mat& Im, const xMarker& Mi) const
 void Interfaces::drawMarkerCircle(Mat& Im, const xMarker& Mi) const
 {
     const Scalar Color = Type2Color(Mi.GetType());
-    const double radius = Mi.GetSize() * SQRT_2 / 2;
+	const double radius = Mi.GetSize() * SQRT_2 / 2;
+	const int 	 r_radius = int(round(radius));
     const double new_angle = (Mi.GetAngle() - 90) * M_PI_180;
-    const Point P1(Mi.GetCenter().x, Mi.GetCenter().y - int(round(radius)));
+    const Point P1(Mi.GetCenter().x, Mi.GetCenter().y - r_radius);
 
 	Point P2;
     P2.x = int(round(Mi.GetCenter().x + radius * cos(new_angle)));
     P2.y = int(round(Mi.GetCenter().y + radius * sin(new_angle)));
 
-    circle(Im, Mi.GetCenter(), radius, _Color, 1);
-    ellipse(Im, Mi.GetCenter(), Size(radius, radius), -90, 0, Mi.GetAngle(), Color, 3);
+    circle(Im, Mi.GetCenter(), r_radius, _Color, 1);
+    ellipse(Im, Mi.GetCenter(), Size(r_radius, r_radius), -90, 0, Mi.GetAngle(), Color, 3);
 	circle(Im, P1, _RadiusButton, _Color, -1);
 	circle(Im, P2, _RadiusButton, Color, -1);
 }
@@ -117,31 +118,63 @@ void Interfaces::drawMarkerCircle(Mat& Im, const xMarker& Mi) const
 //***** Markers Type ****
 void Interfaces::drawSquareMarker(Mat& Im, const xMarker& Mi) const
 {
-    const Scalar Color = Type2Color(Mi.GetType());
-    RotatedRect rotatedRectangle(Mi.GetCenter(), Size2f(Mi.GetSize(), Mi.GetSize()), Mi.GetAngle());
+
+	const Scalar Color = Type2Color(Mi.GetType());
+	const float size = float(Mi.GetSize());
+	const Point Center = Mi.GetCenter();
+	RotatedRect rotatedRectangle(Center, Size2f(size, size), float(Mi.GetAngle()));
+	Rect bbox = rotatedRectangle.boundingRect();
 	Point2f vertices2f[4];
 	rotatedRectangle.points(vertices2f);
 	Point vertices[4];
 	for (int i = 0; i < 4; ++i) vertices[i] = vertices2f[i];
 	fillConvexPoly(Im, vertices, 4, Color);
+
+	if (_Text){
+		const string text = Type2Str(Mi.GetType());
+		const int fontFace = CV_FONT_HERSHEY_SIMPLEX;
+		double fontScale = 2;
+		const int thickness = 2;
+		int baseline = 0;
+		Size textSize = getTextSize(text, fontFace, fontScale, thickness, &baseline);
+		fontScale *= 0.5 * min(bbox.width / textSize.width, bbox.height / textSize.height);
+
+		// center the text
+		Point TextPos = Center - Point(bbox.width / 4, -bbox.height / 4);
+		putText(Im, text, TextPos, fontFace, fontScale, _Color, thickness, 8);
+	}
 }
 
 Scalar Interfaces::Type2Color(MARKER_TYPE T)
 {
 	switch (T) {
-	case TYPE_OSCILL_SINUS:		return COLORS[3];
-	case TYPE_OSCILL_SQUARE:	return COLORS[4];
-	case TYPE_OSCILL_TRIANGLE:	return COLORS[5];
-	case TYPE_OSCILL_SAW:		return COLORS[6];
-	case TYPE_OSCILL_IMPULSE:	return COLORS[7];
-	case TYPE_EFFECT_NOISE:		return COLORS[8];
+	case TYPE_SOURCE_SINUS:		return COLORS[3];
+	case TYPE_SOURCE_NOISE:		return COLORS[4];
+	case TYPE_SOURCE_NUMERICAL:	return COLORS[5];
+	case TYPE_EFFECT_AM:		return COLORS[6];
+	case TYPE_EFFECT_FM:		return COLORS[7];
+	case TYPE_EFFECT_ADD:		return COLORS[8];
 	case TYPE_FILTER_COLOR:		return COLORS[9];
 	default:					return COLORS[2];
 	}
 }
 
+string Interfaces::Type2Str(MARKER_TYPE T)
+{
+	switch (T) {
+		case TYPE_SOURCE_SINUS:		return "S";
+		case TYPE_SOURCE_NOISE:		return "N";
+		case TYPE_SOURCE_NUMERICAL:	return "N";
+		case TYPE_EFFECT_AM:		return "AM";
+		case TYPE_EFFECT_FM:		return "FM";
+		case TYPE_EFFECT_ADD:		return "ADD";
+		case TYPE_FILTER_COLOR:		return "C";
+		default:					return "";
+	}
+}
+
+
 void Interfaces::drawSinMarker(cv::Mat &Im, const xMarker &Mi) const
 {
 
 }
-
